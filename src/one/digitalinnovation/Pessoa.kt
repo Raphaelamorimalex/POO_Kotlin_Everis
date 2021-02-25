0ea5d9447a158c4004957257c97e37e6b1663edf
Raphael.AmorimAlex <Raphael.amorimalex@gmail.com>
2021-02-25 17:07:26 -0300

    Aula 2 POO- PARTE 1

--- src/one/digitalinnovation/Pessoa.kt
+++ src/one/digitalinnovation/Pessoa.kt
@@ -3,6 +3,9 @@ package one.digitalinnovation
 class Pessoa {
     var nome: String = "Raphael Alexander Moura Amorim"
     var cpf:String = "135.204.299-65"
+    /*inner class endereco{
+        var rua:String = "Rua teste"
+    }*/
 }
 
 fun main(){
@@ -10,4 +13,5 @@ fun main(){
 
     println(raphael.nome)
     println(raphael.cpf)
+    //println(raphael.endereco().rua)
 }
